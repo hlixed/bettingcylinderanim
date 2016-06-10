af15118b390fd91403bfa77137cb85c7f8cb0a48
hlixed <onehlixed1@yahoo.com>
2016-06-09 23:12:53 -0400

    Add preliminary color animation; currently just a bad crossfade.

--- js/BettingCylinder.js
+++ js/BettingCylinder.js
@@ -34,19 +34,101 @@ function BettingCylinder(canvas_elem, clear_color, asset_folder){
 	this.scene.add( this.light );
 
 	//add the background gradient
-	var geometry = new THREE.CubeGeometry(100,100,100)
+	var geometry = new THREE.SphereGeometry(100,20,20)
 
 	//color the gradient manually
 	//right face
-	geometry.faces[1].vertexColors = [new THREE.Color(0xafcdd0), new THREE.Color(0xd6f0ff), new THREE.Color(0xe1c1b0)]
 
-	//top left face
-	geometry.faces[10].vertexColors = [new THREE.Color(0xe1c1b0), new THREE.Color(0xd6f0ff), new THREE.Color(0x7a8f91)]
+	//colors: 0xafcdd0, 0xd6f0ff, 0xe1c1b0, 0x7a8f91
 
-	//bottom triangle face
-	geometry.faces[11].vertexColors = [new THREE.Color(0xd6f0ff), new THREE.Color(0xd6f0ff), new THREE.Color(0x7a8f91)]
 
-	this.colorfulbox =  new THREE.Mesh(geometry,new THREE.MeshBasicMaterial({shading: THREE.FlatShading, side: THREE.BackSide, vertexColors: THREE.VertexColors}));
+	// A quick note on the for loop below: a normal SphereGeometry's faces look like this:
+ 	//     |/|/|
+	// ... |/|/| ...
+
+	// the face manipulation is to creae this pattern:  |/|\|
+	//                                                  |\|/|
+
+	
+	for(var rowindex = 20*9; rowindex < 20*29; rowindex += 40){
+		for(var i=0;i<40;i+=4){
+			var index = rowindex + i;
+
+			//without this if-statement, the pattern would be
+			//  |/|\|
+			//  |/|\|/|\|
+			// so every other row, shift the vertices one to the right to make:
+			//  |/|\|     ==  |/|\|
+			//|/|\|/|\|       |\|/|
+			//Technically this leaves an alternating gap because when index+3 is taken below, it's not modded by 40 along the row, so indices 0 and 1 are never reached because of this +2 shift. However, it's not even showing, so I think it's safe.
+			if(rowindex % 80 == 20){
+				index = rowindex + (i+2)%40
+			}
+			//rearrange faces 0,1 mod 4 to form a triangle pattern
+			geometry.faces[index].c = geometry.faces[index+1].b
+			geometry.faces[index+1].a = geometry.faces[index].a
+			
+			geometry.faces[index].vertexColors = [new THREE.Color(0x0000ff), new THREE.Color(0xffffff), new THREE.Color(0x0000ff)];
+			geometry.faces[index+1].vertexColors = [new THREE.Color(0x0000ff), new THREE.Color(0x0000ff), new THREE.Color(0xffffff)];
+			geometry.faces[index+2].vertexColors = [new THREE.Color(0xffffff), new THREE.Color(0x0000ff), new THREE.Color(0x0000ff)];
+			geometry.faces[index+3].vertexColors = [new THREE.Color(0x0000ff), new THREE.Color(0xffffff), new THREE.Color(0x0000ff)];
+			
+		}
+	}
+
+	//experiment: shaders to get the triangle pulsating!
+	var vShader = [
+	"varying vec3 vNormal;",
+	"varying vec3 vPosition;",
+	"varying vec2 vuv;",
+	"varying vec3 vcolor;",
+	"uniform float time;",
+	"vec3 getcolor(float time){",
+		"float lerpfrac = clamp(sin(time),0.0,1.0);",
+		"return vec3(0.68,0.80,0.81) * lerpfrac + vec3(0.88,0.75,0.69) * (1.0-lerpfrac);",
+	"}",
+
+	"void main() {",
+                "vcolor = getcolor(time + color.r);",
+		"//vcolor = color;",
+		"vPosition = position.xyz;",
+		"vNormal = normal.xyz;",
+  		"gl_Position = projectionMatrix *",
+                "modelViewMatrix *",
+                "vec4(position,1.0);",
+	"}"].join("\n")
+
+	var fShader = [
+	"varying vec3 vNormal;",
+	"varying vec3 vPosition;",
+	"varying vec2 vuv;",
+	"varying vec3 vcolor;",
+	"uniform float time;",
+	"void main(){",
+	"  //gl_FragColor = vec4(clamp(sin(vPosition.x),0.0,1.0), clamp(sin(vPosition.z),0.0,1.0), 1.0, 1.0);", //A
+	"  //gl_FragColor = vec4(vNormal.x,vNormal.y, vNormal.z, 1.0);",
+	"  gl_FragColor = vec4(vcolor.rgb, 1.0);", 
+	"}"].join("\n")
+
+	var uniforms = {
+		time: {
+			type: 'f',
+			value: 0,
+		}
+	};
+
+	this.colorfulbox =  new THREE.Mesh(
+		geometry,
+		new THREE.ShaderMaterial({
+			side: THREE.BackSide,
+			vertexShader: vShader, 
+			fragmentShader: fShader,
+			vertexColors: THREE.VertexColors,
+			uniforms: uniforms,
+			})
+		);
+
+	this.colorfulbox.uniforms = uniforms;
 	this.scene.add( this.colorfulbox );
 
                                                                                
@@ -98,6 +180,9 @@ BettingCylinder.prototype.update = function(delta){
 		this.circles[i].update(delta);
 		//update circles
 	}
+
+	//update BG
+	this.colorfulbox.uniforms.time.value = this.animtimer;
 
 	//if any circles are dead
 
